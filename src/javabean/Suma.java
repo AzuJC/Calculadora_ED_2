28eda771f5e4f555a67211ba93d1a713070dfff2
alexanderromandiaz <alexander.roman981@comunidadunir.net>
2025-01-25 11:16:49 +0100

    Suma Añadida

--- src/javabean/Suma.java
+++ src/javabean/Suma.java
@@ -1,6 +1,14 @@
 package javabean;
 
 // Clase para operaciones de suma
+/*
+ Esta clase implementará todos los métodos que están relacionados con la suma de la calculadora. Esta
+clase tendrá los siguientes métodos:
+1. Suma de dos números reales, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+2. Suma de dos números enteros, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+3. Suma de tres números reales, tendrá 3 parámetros de entrada y uno de salida que será la solución.
+
+* */
 class Suma {
     private double acumulado = 0;
 
